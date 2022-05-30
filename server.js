c7d984f5b197bb1500f94a7c90b5a8206aa37059
Richard Guarnieri <richard.gm@outlook.com>
2022-05-30 00:30:58 -0400

    created db connection

--- server.js
+++ server.js
@@ -2,12 +2,25 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
-console.table([
-    {
-      name: 'foo',
-      age: 10
-    }, {
-      name: 'bar',
-      age: 20
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "employee_tracker"
+});
+
+connection.query("SELECT * FROM department", (err, results) => {
+    if (err) {
+        console.log(err);
     }
-  ]);
+    console.log(results)
+})
+// console.table([
+//     {
+//       name: 'foo',
+//       age: 10
+//     }, {
+//       name: 'bar',
+//       age: 20
+//     }
+//   ]);
