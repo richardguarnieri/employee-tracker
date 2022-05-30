dfee41f4aaf595f6442841c1eaf707c223a15e17
Richard Guarnieri <richard.gm@outlook.com>
2022-05-30 09:21:56 -0400

    added "AS" keyword on "View All Roles" query

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ INNER JOIN employee_tracker.department ON role.department_id = department.id;
 // View All Roles
 connection.query(`
 SELECT employee1.id, employee1.first_name, employee1.last_name, role.title, department.name AS department, role.salary, CONCAT(employee2.first_name, ' ', employee2.last_name) AS manager
-FROM employee_tracker.employee employee1
-LEFT JOIN employee_tracker.employee employee2 ON employee1.manager_id = employee2.id
+FROM employee_tracker.employee AS employee1
+LEFT JOIN employee_tracker.employee AS employee2 ON employee1.manager_id = employee2.id
 INNER JOIN employee_tracker.role ON employee1.role_id = role.id
 INNER JOIN employee_tracker.department ON role.department_id = department.id;
 `, (err, results) => {
