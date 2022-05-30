f28ef254b16cbf445ac439fc30667ed3f2e271a0
Richard Guarnieri <richard.gm@outlook.com>
2022-05-29 23:59:07 -0400

    updates to schema.sql - added CONSTRAINT statement

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE employee_tracker;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    CONSTRAINT pk_department PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
@@ -15,8 +15,8 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    CONSTRAINT pk_role PRIMARY KEY (id),
+    CONSTRAINT fk_role_department_id FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -24,6 +24,8 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    PRIMARY KEY (id)
-    FOREIGN KEY (role_id) REFERENCES role(id)
+    manager_id INT,
+    CONSTRAINT pk_employee PRIMARY KEY (id),
+    CONSTRAINT fk_employee_role_id FOREIGN KEY (role_id) REFERENCES role(id),
+    CONSTRAINT fk_employee_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
