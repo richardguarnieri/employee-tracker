b4b2560c54e9dd4fff8a9a976a8226b28c5b889a
Richard Guarnieri <richard.gm@outlook.com>
2022-05-30 10:07:10 -0400

    drafting the inquirer logic

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const connection = mysql.createConnection({
 });
 
 // View All Departments
-connection.query(`SELECT * FROM employee_tracker.department`, (err, results) => {
+const viewAllDepartments = connection.query(`SELECT * FROM employee_tracker.department`, (err, results) => {
     err ? console.log(err) : console.table(results)
 });
 
 // View All Roles
-connection.query(`
+const viewAllRoles = connection.query(`
 SELECT role.id, role.title, department.name AS department, role.salary
 FROM employee_tracker.role
 INNER JOIN employee_tracker.department ON role.department_id = department.id;
@@ -23,8 +23,8 @@ INNER JOIN employee_tracker.department ON role.department_id = department.id;
     err ? console.log(err) : console.table(results)
 });
 
-// View All Roles
-connection.query(`
+// View All Employees
+const viewAllEmployees = connection.query(`
 SELECT employee1.id, employee1.first_name, employee1.last_name, role.title, department.name AS department, role.salary, CONCAT(employee2.first_name, ' ', employee2.last_name) AS manager
 FROM employee_tracker.employee AS employee1
 LEFT JOIN employee_tracker.employee AS employee2 ON employee1.manager_id = employee2.id
@@ -33,3 +33,15 @@ INNER JOIN employee_tracker.department ON role.department_id = department.id;
 `, (err, results) => {
     err ? console.log(err) : console.table(results)
 });
+
+
+
+
+
+const welcomeFn = async () => {
+    console.log(`Employee Manager`);
+    inquirer.prompt({
+    })
+}
+
+welcomeFn();
