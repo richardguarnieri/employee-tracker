48a68b1b5af1c125e6c43149a1aa36d41b70d0ba
Richard Guarnieri <richard.gm@outlook.com>
2022-05-30 00:11:24 -0400

    added initial department seed

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+-- Create Initial Seeds
+INSERT INTO department (name)
+    VALUES 
+        ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
+
+INSERT INTO department (name)
+    VALUES 
+        ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
+
+INSERT INTO department (name)
+    VALUES 
+        ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
+
+
