895325b8ce031b825a0ce4b8e241ebf5440ea20e
Richard Guarnieri <richard.gm@outlook.com>
2022-05-30 00:16:39 -0400

    added initial role seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,12 +6,17 @@ INSERT INTO department (name)
         ("Finance"),
         ("Legal");
 
-INSERT INTO department (name)
+INSERT INTO role (title, salary, department_id)
     VALUES 
-        ("Sales"),
-        ("Engineering"),
-        ("Finance"),
-        ("Legal");
+        ("Sales Lead", 100000, 1),
+        ("Salesperson", 80000, 1),
+        ("Lead Engineer", 150000, 2),
+        ("Software Engineer", 120000, 2),
+        ("Account Manager", 160000, 3),
+        ("Accountant", 125000, 3),
+        ("Legal Team Lead", 250000, 4),
+        ("Lawyer", 190000, 4),
+        
 
 INSERT INTO department (name)
     VALUES 
