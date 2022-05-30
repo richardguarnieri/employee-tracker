0b7e1aa89e3fa0926a6f19d094295801a76be7f3
Richard Guarnieri <richard.gm@outlook.com>
2022-05-29 23:47:34 -0400

    created schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+-- Drop, Create and Use "employee_tracker" Database
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+USE employee_tracker;
+
+-- Create Tables
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY (id)
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
