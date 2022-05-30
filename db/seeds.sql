712b80015ab093689367f90ab14b98d77265f7d1
Richard Guarnieri <richard.gm@outlook.com>
2022-05-30 00:26:39 -0400

    updated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -20,11 +20,11 @@ INSERT INTO role (title, salary, department_id)
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
     VALUES 
-        ("John", "Doe", 1),
+        ("John", "Doe", 1, null),
         ("Mike", "Chan", 2, 1),
-        ("Ashley", "Rodriguez", 3),
+        ("Ashley", "Rodriguez", 3, null),
         ("Kevin", "Tupik", 4, 3),
-        ("Kunak", "Singh", 5),
+        ("Kunak", "Singh", 5, null),
         ("Malia", "Brown", 6, 5),
-        ("Sarah", "Lourd", 7),
+        ("Sarah", "Lourd", 7, null),
         ("Tom", "Allen", 8, 7);
