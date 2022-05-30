15d8a714bf266abb6f3104d9ec44cb11b317b18d
Richard Guarnieri <richard.gm@outlook.com>
2022-05-30 01:38:45 -0400

    updated server.js with connection queries

--- server.js
+++ server.js
@@ -13,10 +13,22 @@ const connection = mysql.createConnection({
 connection.query(`SELECT * FROM employee_tracker.department`, (err, results) => {
     err ? console.log(err) : console.table(results)
 });
+
 // View All Roles
 connection.query(`
 SELECT role.id, role.title, department.name AS department, role.salary
 FROM employee_tracker.role
+INNER JOIN employee_tracker.department ON role.department_id = department.id;
+`, (err, results) => {
+    err ? console.log(err) : console.table(results)
+});
+
+// View All Roles
+connection.query(`
+SELECT employee1.id, employee1.first_name, employee1.last_name, role.title, department.name AS department, role.salary, CONCAT(employee2.first_name, ' ', employee2.last_name) AS manager
+FROM employee_tracker.employee employee1
+LEFT JOIN employee_tracker.employee employee2 ON employee1.manager_id = employee2.id
+INNER JOIN employee_tracker.role ON employee1.role_id = role.id
 INNER JOIN employee_tracker.department ON role.department_id = department.id;
 `, (err, results) => {
     err ? console.log(err) : console.table(results)
