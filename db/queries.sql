be74a482be56d3646dd98493f824761250a70883
Richard Guarnieri <richard.gm@outlook.com>
2022-05-30 01:37:44 -0400

    added "View All Employees" query

--- db/queries.sql
+++ db/queries.sql
@@ -6,7 +6,9 @@ SELECT role.id, role.title, department.name AS department, role.salary
 FROM employee_tracker.role
 INNER JOIN employee_tracker.department ON role.department_id = department.id;
 
--- View All Employees
-SELECT role.id, role.title, department.name AS department, role.salary
-FROM employee_tracker.role
+-- View All Employees (Incl. Self Join)
+SELECT employee1.id, employee1.first_name, employee1.last_name, role.title, department.name AS department, role.salary, CONCAT(employee2.first_name, ' ', employee2.last_name) AS manager
+FROM employee_tracker.employee employee1
+LEFT JOIN employee_tracker.employee employee2 ON employee1.manager_id = employee2.id
+INNER JOIN employee_tracker.role ON employee1.role_id = role.id
 INNER JOIN employee_tracker.department ON role.department_id = department.id;
