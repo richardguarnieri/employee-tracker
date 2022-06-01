ee76513f2f9a51f11149da99e580eb7c3d972603
Richard Guarnieri <richard.gm@outlook.com>
2022-05-31 20:02:23 -0400

    added two more queries

--- db/queries.sql
+++ db/queries.sql
@@ -31,8 +31,17 @@ SET employee_tracker.employee.role_id = ?
 WHERE employee_tracker.employee.id = ?;
 
 -- Update Employee Manager
+UPDATE employee_tracker.employee
+SET employee_tracker.employee.manager_id = ?
+WHERE employee_tracker.employee.id = ?;
 
 -- View Employees by Manager
+SELECT employee1.id, employee1.first_name, employee1.last_name, role.title, department.name AS department, role.salary, CONCAT(employee2.first_name, ' ', employee2.last_name) AS manager
+FROM employee_tracker.employee AS employee1
+LEFT JOIN employee_tracker.employee AS employee2 ON employee1.manager_id = employee2.id
+INNER JOIN employee_tracker.role ON employee1.role_id = role.id
+INNER JOIN employee_tracker.department ON role.department_id = department.id
+WHERE employee1.manager_id = ?;
 
 -- View Employees by Department
 
